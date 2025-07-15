6fb9cdc2a4b2a78405911583a36be9cac9aee21e
jorgegsosa11 <199567664+jorgegsosa11@users.noreply.github.com>
2025-07-14 21:14:04 -0700

    Update WN10-CC-000030.ps1

--- STIGS/WN10-CC-000030.ps1
+++ STIGS/WN10-CC-000030.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the 'EnableICMPRedirect' registry setting is configured properly to prevent ICMP redirect-based route manipulation.
+
+.DESCRIPTION
+    Following ICMP redirect of routes can lead to traffic not being routed properly. 
+    When disabled, this forces ICMP to be routed via shortest path first. 
+    This script checks and enforces the setting: 
+    HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\EnableICMPRedirect = 0
+
+.NOTES
+    Author          : Jorge Garcia Sosa
+    GitHub          : github.com/jorgegsosa11
+    Date Created    : 2025-07-15
+    Last Modified   : 2025-07-15
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000030
+#>
+
+
+
 # Define registry parameters
 $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"
 $valueName = "EnableICMPRedirect"
